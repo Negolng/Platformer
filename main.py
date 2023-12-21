093b4e2f3041e8efc411dd00831c8a80f08e3da5
Negolng <114674976+Negolng@users.noreply.github.com>
2023-12-21 16:36:14 +0500

    Removed unused logic

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
 from Objects import Ball, screen, dispy, fps, MainCharacter, Border, BetterGroup, Box, AI, Platform, Cursor
-from Gravitron import BlackHole
 import pygame
-import random
-
 
 if __name__ == '__main__':
-    godmod = False
     running = True
 
     clock = pygame.time.Clock()
@@ -45,8 +41,6 @@ if __name__ == '__main__':
     pygame.mixer.music.load('music/Homicide.mp3')
     pygame.mixer.music.play(-1)
 
-
-    throw_power = 3
     while running:
         print(character.rect.x, character.rect.y)
         screen.fill((255, 255, 255))
@@ -74,52 +68,6 @@ if __name__ == '__main__':
             if event.type == pygame.MOUSEBUTTONDOWN:
                 pass
                 # Particles(particles_group, pygame.mouse.get_pos())
-
-            if godmod:
-                if event.type == pygame.MOUSEBUTTONDOWN:
-                    gravipopa.append(BlackHole(pygame.mouse.get_pos(), 6000, 15, dispy))
-                elif event.type == pygame.KEYDOWN:
-                    keys = pygame.key.get_pressed()
-                    if keys[pygame.K_DELETE]:
-                        sprites = pygame.sprite.Group()
-                        gravipopa = []
-                    elif keys[pygame.K_i]:
-                        dispy.gravity_direction = 3
-                    elif keys[pygame.K_j]:
-                        dispy.gravity_direction = 2
-                    elif keys[pygame.K_k]:
-                        dispy.gravity_direction = 1
-                    elif keys[pygame.K_l]:
-                        dispy.gravity_direction = 4
-                    elif keys[pygame.K_RIGHT]:
-                        Ball(dispy, pygame.mouse.get_pos(), 15, fps * throw_power, 0, gravipopa, other_sprites)
-                    elif keys[pygame.K_LEFT]:
-                        Ball(dispy, pygame.mouse.get_pos(), 15, -fps * throw_power, 0, gravipopa, other_sprites)
-                    elif keys[pygame.K_UP]:
-                        Ball(dispy, pygame.mouse.get_pos(), 15, 0, -fps * throw_power, gravipopa, other_sprites)
-                    elif keys[pygame.K_DOWN]:
-                        Ball(dispy, pygame.mouse.get_pos(), 15, 0, fps * throw_power, gravipopa, other_sprites)
-
-                    elif keys[pygame.K_1]:
-                        throw_power = 1
-                    elif keys[pygame.K_2]:
-                        throw_power = 2
-                    elif keys[pygame.K_3]:
-                        throw_power = 3
-                    elif keys[pygame.K_4]:
-                        throw_power = 4
-                    elif keys[pygame.K_5]:
-                        throw_power = 5
-                    elif keys[pygame.K_6]:
-                        throw_power = 6
-                    elif keys[pygame.K_7]:
-                        throw_power = 7
-                    elif keys[pygame.K_8]:
-                        throw_power = 8
-                    elif keys[pygame.K_9]:
-                        throw_power = 9
-                    elif keys[pygame.K_0]:
-                        throw_power = 0
 
         other_sprites.draw(dispy.display)
         other_sprites.update()
